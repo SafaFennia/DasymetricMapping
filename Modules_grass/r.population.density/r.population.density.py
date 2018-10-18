ccc59bcc77778b58075aaaad0e1f421ca5a5f02b
tais <tgrippa@ulb.ac.be>
2018-10-18 15:55:21 +0200

    Minor fix to allow the module to handle raster data with nominal categories

--- Modules_grass/r.population.density/r.population.density.py
+++ Modules_grass/r.population.density/r.population.density.py
@@ -225,7 +225,7 @@ def Data_prep(categorical_raster):
     nsres=info.nsres
     ewres=info.ewres
     L = []
-    L=[cl for cl in gscript.parse_command('r.category',map=categorical_raster)]
+    L=[cl.split("	")[0] for cl in gscript.parse_command('r.category',map=categorical_raster)]
     for i,x in enumerate(L):  #Make sure the format is UTF8 and not Unicode
         L[i]=x.encode('UTF8')
     L.sort(key=float) #Sort the raster categories in ascending.
